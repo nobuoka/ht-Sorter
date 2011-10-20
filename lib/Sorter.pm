679b33fd4e14cdb74c5bbee07d1c6202f45c5070
nobuoka <nobuoka@r-definition.com>
2011-10-21 03:14:52 +0900

    implemented my Sorter.pm

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,1 +1,71 @@
 # implement this
+
+use strict;
+use warnings;
+
+my $test = { "good" => { "test" => 5 } };
+my %h = %{$test->{"good"}};
+print $h{"test"} . "\n";
+
+package Sorter;
+
+sub new {
+    my $class = shift;
+    return bless {
+        "values" => []
+    }, $class;
+}
+
+sub set_values {
+    my $self = shift;
+    $self->{"values"} = [@_];
+}
+
+sub sort {
+    my $self = shift;
+    #$self->{"values"} = [ sort{ $a <=> $b }( @{$self->{"values"}} ) ];
+    my $len = @{$self->{"values"}};
+    if( $len > 1 ) {
+        _sort_by_quick_sort( $self->{"values"}, 0, $len - 1 );
+    }
+}
+
+sub get_values {
+    my $self = shift;
+    return @{$self->{"values"}};
+}
+
+sub _sort_by_quick_sort {
+    my ( $aref, $bidx, $eidx ) = @_;
+    # 要素数が 1 の場合
+    if( $bidx == $eidx ) {
+        return;
+    }
+    # 要素数が 2 の場合
+    if( $bidx + 1 == $eidx ) {
+        if( $aref->[$bidx] > $aref->[$eidx] ) { 
+            ( $aref->[$bidx], $aref->[$eidx] ) = ( $aref->[$eidx], $aref->[$bidx] ); 
+        }
+        return;
+    }
+    # 要素数が 3 以上の場合
+    my $i2 = $bidx;
+    my $j2 = $eidx;
+    my $p = $aref->[ $bidx + ( $eidx - $bidx ) / 2 ];
+    while( 1 ) {
+        my ( $i, $j );
+        for( $i = $i2; $i < $eidx and $aref->[$i] < $p; ++ $i ) {} 
+        for( $j = $j2; $bidx < $j and $p < $aref->[$j]; -- $j ) {} 
+        if( $i < $j ) {
+            ( $aref->[$i], $aref->[$j] ) = ( $aref->[$j], $aref->[$i] );
+            $i2 = $i + 1;
+            $j2 = $j - 1;
+        } else {
+            &_sort_by_quick_sort( $aref, $bidx, $i - 1 );
+            &_sort_by_quick_sort( $aref, $i, $eidx );
+            last;
+        }
+    }
+}
+
+1;
