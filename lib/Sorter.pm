11b3e53d2a2840c2e2fffc1fdf15dce5f4247202
nobuoka <nobuoka@r-definition.com>
2011-11-06 07:40:28 +0900

    wrote pod document

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,7 +1,42 @@
 use strict;
 use warnings;
+use utf8;
 
 package Sorter;
+
+=head1 NAME
+
+Sorter -- 数値的に値をソートする
+
+=head1 SYNOPSIS
+
+  use Sorter;
+
+  my $s = Sorter->new();
+  $s->set_values( 1, 3, 2 );
+  $s->get_values(); #=> 1, 3, 2
+  $s->sort();
+  $s->get_values(); #=> 1, 2, 3
+
+  $s->add_values( 0, 2 );
+  $s->get_values(); #=> 1, 2, 3, 0, 2
+
+=head1 DESCRIPTION
+
+このクラスは, 与えられた値を数値的に比較してソートします. 
+Sorter オブジェクトはソート対象の値を保持するリストを内部に持ちます. 
+
+=head2 Class methods
+
+=over
+
+=item Sorter->new()
+
+内部に空のリストを持つ Sorter オブジェクトを生成します. 
+
+=back
+
+=cut
 
 sub new {
     my $class = shift;
@@ -9,6 +44,30 @@ sub new {
         "values" => []
     }, $class;
 }
+
+=head2 Instance methods
+
+=over
+
+=item $sorter->set_values( LIST )
+
+内部のリストを空にして, 引数で与えられた LIST の各値を内部のリストに放り込みます.  
+
+=item $sorter->add_values( LIST )
+
+引数で与えられた LIST の各値を内部のリストに放り込みます. 
+
+=item $sorter->sort()
+
+内部のリスト内の値を数値的に比較してソートします. 
+
+=item $sorter->get_values()
+
+内部のリストの値を返します.
+
+=back
+
+=cut
 
 sub set_values {
     my $self = shift;
